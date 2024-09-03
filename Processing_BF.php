60201f0c956776979cf0751d95c6d3b7cf62f71f
Evgeny Stepanischev <imbolk@gmail.com>
2024-09-03 09:33:26 +0300

    strict types, php8

--- Processing_BF.php
+++ Processing_BF.php
@@ -1,72 +1,37 @@
 <?php
-/* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4: */
-/**
- *
- * Optimizing compilator from Brainfuck to PHP
- *
- * PHP version 5.5+
- *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
- * @category   Processing
- * @package    Processing_BF
- * @author     Evgeny Stepanischev <imbolk@gmail.com>
- * @copyright  2005-2014 Evgeny Stepanischev
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    1.2
- */
-
-// {{{ class Processing_BF
+declare(strict_types=1);
 
 class Processing_BF
 {
     /**
-     * Language extensions
-     */
-    private $opts = [];
-
-    // {{{ compile()
-    /**
-     * Program compling
-     * @param string $str   BF program code
+     * Program compiling
+     * @param string $str BF program code
      * @param string $input input data of BF program
-     * @param bool   $y     enable Y-operator
-     *
      * @return string PHP code
-     *
      */
-    public function compile($str, $input = '', $extensions = [])
+    public function compile(string $str, string $input = ''): string
     {
-        $this->opts = $extensions;
         return $this->addHeader($this->toPHP($str), $input);
     }
-    // }}}
 
-    // {{{ addHeader()
     /**
-     * Add standart header to compiled BF program
-     * @param string $str   compiled BF program
+     * Add standard header to compiled BF program
+     * @param string $str compiled BF program
      * @param string $input input data of BF program
      *
      * @return string PHP code
      *
      */
-    public function addHeader($str, $input = '')
+    public function addHeader(string $str, string $input = ''): string
     {
-        $str = "\$d=array_fill(0, 65535 * 2, 0); \$i=count(\$d)/2; ".$str;
+        $str = '$d=array_fill(0, 65535 * 2, 0); $i=count($d)/2; ' . $str;
 
-        // If input isn't empty we convert it to array of charcodes
-        $codes = $input == '' ? [] : unpack('c*', $input . "\0");
+        // If input isn't empty we convert it to array of char codes
+        $codes = $input === '' ? [] : unpack('c*', $input . "\0");
 
-        return '$in=['.implode(', ', $codes).']; '.$str;
+        return '$in=[' . implode(', ', $codes) . ']; ' . $str;
     }
-    // }}}
 
-    // {{{ toPHP()
     /**
      * Compile program to PHP
      * @param string $str BF program code
@@ -74,13 +39,10 @@ class Processing_BF
      * @return string PHP code
      *
      */
-    public function toPHP($str)
+    public function toPHP(string $str): string
     {
         return $this->_compile($this->_prepare($str));
     }
-    // }}}
-
-    // {{{ _prepare()
 
     /**
      * Preparing to compiling and starting.
@@ -89,11 +51,10 @@ class Processing_BF
      * @return string prepared program code
      *
      */
-    protected function _prepare($str)
+    protected function _prepare(string $str): string
     {
         // Remove trash chars
-        $opts = implode($this->opts);
-        $str = preg_replace("/[^\-\+\[\]><\,\.{$opts}]/", '', $str);
+        $str = preg_replace("/[^\-+\[\]><,.]+/", '', $str);
 
         // Escaping opcodes
         $trans = [
@@ -101,13 +62,13 @@ class Processing_BF
             '[>]' => 'r',
             '[-]' => 'c',
             '[+]' => 'c',
-            '+'   => 'P',
-            '-'   => 'M',
-            '<'   => 'm',
-            '>'   => 'p',
-            '['   => 'L',
-            ']'   => 'R',
-            '.'   => 'E',
+            '+' => 'P',
+            '-' => 'M',
+            '<' => 'm',
+            '>' => 'p',
+            '[' => 'L',
+            ']' => 'R',
+            '.' => 'E',
         ];
 
         $str = strtr($str, $trans);
@@ -118,9 +79,9 @@ class Processing_BF
         // group + and -, > and <
         foreach (['MP', 'mp'] as $set) {
             $str = preg_replace_callback("/[$set]{2,}/S",
-                function ($m) {
+                static function ($m) {
                     $freq = count_chars($m[0], 1);
-                    if (count($freq) == 2) {
+                    if (count($freq) === 2) {
                         arsort($freq);
 
                         $winner = chr(key($freq));
@@ -135,128 +96,114 @@ class Processing_BF
 
                     return $m[0];
                 },
-            $str);
+                $str);
         }
 
         // repeating opcodes
-        $result = preg_replace_callback('/([PMpm])(\\1{1,98})/', function ($m) {
-            // Callback for repeating opcodes replacement
+        return preg_replace_callback('/([PMpm])(\\1{1,98})/', // Callback for repeating opcodes replacement
             // sq. length
-            return sprintf('%02d%s', strlen($m[2]) + 1, $m[1]);
-        }, $str);
-
-        return $result;
+            static fn($m) => sprintf('%02d%s', strlen($m[2]) + 1, $m[1]), $str);
     }
-    // }}}
-    // {{{ _dir_op
 
     /**
      * Complex opcodes transformation
-     * @param string $dir   move direction
-     * @param int    $shift number of movement
-     * @param int    $col   number for sub or add
-     * @param string $op    operation - sub (M) or add (P)
+     * @param string $dir move direction
+     * @param string|int $shift number of movement
+     * @param string $col number for sub or add
+     * @param string $op operation - sub (M) or add (P)
      *
      * @return string prepared program code
-     *
      */
-    protected function _dir_op($dir, $shift, $col, $op)
+    protected function _dir_op(string $dir, string|int $shift, string $col, string $op): string
     {
-        $dir   = $dir == 'm' ? '-' : '+';
-        $shift = $shift ? (int) $shift : 1;
+        $dir = $dir === 'm' ? '-' : '+';
+        $shift = $shift ? (int)$shift : 1;
 
-        $shift = '$i'.$dir.intval($shift);
+        $shift = '$i' . $dir . $shift;
 
-        if ($op == 'c') {
+        if ($op === 'c') {
             $op = '=0';
         } else {
-            $op = $op == 'M' ? '-' : '+';
+            $op = $op === 'M' ? '-' : '+';
 
             if ($col) {
-                $op = $op.'='.(int) $col;
+                $op .= '=' . (int)$col;
             } else {
-                $op = $op.$op;
+                $op .= $op;
             }
         }
 
-        return '$d['.$shift.']'.$op.';';
+        return '$d[' . $shift . ']' . $op . ';';
     }
-    // }}}
-    // {{{ _op()
 
     /**
      * Simple opcodes transformation
-     * @param int $repeat operation repeat factor
-     * @param int $op     opcode
-     * @param int $idx    optional data index string
+     * @param string|int $repeat operation repeat factor
+     * @param string $op opcode
+     * @param string|bool $idx optional data index string
      *
      * @return string prepared program code
-     *
      */
-    protected function _op($repeat, $op, $idx = false)
+    protected function _op(string|int $repeat, string $op, string|bool $idx = false): string
     {
-        $idx = $idx === false ? '$d[$i]' : '$d['.$idx.']';
+        $idx = $idx === false ? '$d[$i]' : '$d[' . $idx . ']';
 
-        if ($repeat = (int) $repeat) {
+        $repeat = (int)$repeat;
+
+        if ($repeat) {
             switch ($op) {
-               case 'M':
-                   return $idx.'-='.$repeat.';';
-               case 'P':
-                   return $idx.'+='.$repeat.';';
-               case 'c':
-                   return $idx.'=0;';
-               case 'm':
-                   return '$i-='.$repeat.';';
-               case 'p':
-                   return '$i+='.$repeat.';';
-           }
+                case 'M':
+                    return $idx . '-=' . $repeat . ';';
+                case 'P':
+                    return $idx . '+=' . $repeat . ';';
+                case 'c':
+                    return $idx . '=0;';
+                case 'm':
+                    return '$i-=' . $repeat . ';';
+                case 'p':
+                    return '$i+=' . $repeat . ';';
+            }
         } else {
             switch ($op) {
-               case 'M':
-                   return $idx.'--;';
-               case 'P':
-                   return $idx.'++;';
-               case 'c':
-                   return $idx.'=0;';
-               case 'm':
-                   return '--$i;';
-               case 'p':
-                   return '++$i;';
-           }
+                case 'M':
+                    return $idx . '--;';
+                case 'P':
+                    return $idx . '++;';
+                case 'c':
+                    return $idx . '=0;';
+                case 'm':
+                    return '--$i;';
+                case 'p':
+                    return '++$i;';
+            }
         }
 
-        return str_repeat($op, 1 + (int) $repeat);
+        return str_repeat($op, 1 + $repeat);
     }
-    // }}}
-    // {{{ _apply_divider
-
 
     /**
      * Apply loop divider to code
-     * @param string $out     code string
-     * @param int    $divider divider value (0 = no divider)
+     * @param string $out code string
+     * @param int $divider divider value (0 = no divider)
      *
      * @return string prepared program code
      *
      */
-    protected function _apply_divider($out, $divider)
+    protected function _apply_divider(string $out, int $divider): string
     {
         $divider = $divider ?: 1;
 
-        $out = preg_replace_callback('/\*\((\d+)\)/S', function ($m) use ($divider) {
+        return preg_replace_callback('/\*\((\d+)\)/S', static function ($m) use ($divider) {
             $num = $m[1] / $divider;
 
             if ($num === 1) {
                 return '';
             }
 
-            return $num == (int) $num ? '*'.$num : '/'.(1 / $num);
+            return $num === (float)(int)$num ? '*' . $num : '/' . (1 / $num);
         }, $out);
-
-        return $out;
     }
 
-    // {{{ _cycles_op
     /**
      * Cycles optimization
      * @param string $str string to optimization
@@ -264,19 +211,19 @@ class Processing_BF
      * @return string prepared program code
      *
      */
-    protected function _cycles_op($str)
+    protected function _cycles_op(string $str): string
     {
         // Is loop entry point concur with exit point?
-        $brack = ['m' => 0, 'p' => 0];
+        $braces = ['m' => 0, 'p' => 0];
 
         preg_replace_callback('/(\d{2}|)([mp])/S',
-            function ($m) use (&$brack) {
-                $brack[$m[2]] += $m[1] ?: 1;
+            static function ($m) use (&$braces) {
+                $braces[$m[2]] += $m[1] ?: 1;
             },
-        $str);
+            $str);
 
-        if ($brack['m'] != $brack['p']) {
-            return 'L'.$str.'R';
+        if ($braces['m'] !== $braces['p']) {
+            return 'L' . $str . 'R';
         }
 
         // Execution emulation
@@ -288,38 +235,38 @@ class Processing_BF
         $divider = 0;
 
         $len = strlen($str);
-        for ($i = 0; $i<$len; $i++) {
-            if ((string) (int) $str{$i} === (string) $str{$i}) {
-                $num = (int) substr($str, $i++, 2);
-                $op  = $str{++$i};
+        for ($i = 0; $i < $len; $i++) {
+            if ((string)(int)$str[$i] === $str[$i]) {
+                $num = (int)substr($str, $i++, 2);
+                $op = $str[++$i];
             } else {
                 $num = 1;
-                $op  = $str{$i};
+                $op = $str[$i];
             }
 
             if ($pos > 0) {
-                $pos = '+'.(int) $pos;
+                $pos = '+' . $pos;
             } elseif (!$pos) {
                 $pos = '';
             }
 
             switch ($op) {
                 case 'm':
-                    $pos -= $num;
+                    $pos = (int)$pos - $num;
                     break;
 
                 case 'p':
-                    $pos += $num;
+                    $pos = (int)$pos + $num;
                     break;
 
                 case 'M':
                 case 'P':
                     if ($start || $pos) {
-                        $op  = $op == 'M' ? '-' : '+';
+                        $op = $op === 'M' ? '-' : '+';
 
-                        $out .= '$d[$i'.$pos.']'.$op.'=abs($d[$i])*('.$num.');';
+                        $out .= '$d[$i' . $pos . ']' . $op . '=abs($d[$i])*(' . $num . ');';
                     } else {
-                        $start = $pos == 0;
+                        $start = $pos === 0;
 
                         if ($start) {
                             $divider += $num;
@@ -330,7 +277,7 @@ class Processing_BF
 
                 case 'c':
                     if ($pos) {
-                        $out .= 'if ($d[$i]) $d[$i'.$pos.']=0;';
+                        $out .= 'if ($d[$i]) $d[$i' . $pos . ']=0;';
                     } else {
                         $out .= '$d[$i]=0;';
                         $start = true;
@@ -345,83 +292,61 @@ class Processing_BF
                 $out .= '$d[$i]=0;';
             }
 
-            $out = $this->_apply_divider($out, $divider);
-
-            return $out;
+            return $this->_apply_divider($out, $divider);
         }
 
-        return 'L'.$str.'R';
+        return 'L' . $str . 'R';
     }
-    // }}}
-    // {{{ _compile()
 
     /**
-     * Main complile routine
+     * Main compile routine
      * @param string $str program data
      *
      * @return string PHP program code
      *
      */
-    protected function _compile($str)
+    protected function _compile(string $str): string
     {
         $repl = [
             // [>>>+<<-<]
-            '/L([MPmpc\d]+)R/' => function ($m) {
-                return $this->_cycles_op($m[1]);
-            },
+            '/L([MPmpc\d]+)R/' => fn($m) => $this->_cycles_op($m[1]),
 
             // <+++>, <[-]>, <--->
-            '/(\d{2}|(?<!\d))(m)(\d{2}|)([McP])\\1p/' => function ($m) {
-                return $this->_dir_op($m[2], $m[1], $m[3], $m[4]);
-            },
+            '/(\d{2}|(?<!\d))(m)(\d{2}|)([McP])\\1p/' =>
+                fn($m) => $this->_dir_op($m[2], $m[1], $m[3], $m[4]),
 
             // >+++<, >[-]<. >---<
-            '/(\d{2}|(?<!\d))(p)(\d{2}|)([McP])\\1m/' => function ($m) {
-                return $this->_dir_op($m[2], $m[1], $m[3], $m[4]);
-            },
+            '/(\d{2}|(?<!\d))(p)(\d{2}|)([McP])\\1m/' =>
+                fn($m) => $this->_dir_op($m[2], $m[1], $m[3], $m[4]),
 
             // ++>>, <<<-
-            '/(\d{2}|(?<!\d))([MP])([mp])/' => function ($m) {
-                return $this->_op($m[1], $m[2], $m[3] === "m" ? '$i--' : '$i++');
-            },
+            '/(\d{2}|(?<!\d))([MP])([mp])/' =>
+                fn($m) => $this->_op($m[1], $m[2], $m[3] === "m" ? '$i--' : '$i++'),
 
             // <<+, >>>-, >>>[-]
-            '/(\d{2}|(?<!\d))([pm])(\d{2}|)([PMc])/' => function ($m) {
-                return $this->_op($m[3], $m[4], rtrim($this->_op($m[1], $m[2]), ";"));
-            },
+            '/(\d{2}|(?<!\d))([pm])(\d{2}|)([PMc])/' =>
+                fn($m) => $this->_op($m[3], $m[4], rtrim($this->_op($m[1], $m[2]), ";")),
 
             // ++, ---, [-], [<], [>], <<<, >>>
-            '/(\d{2}|)([MPmplrc])/' => function ($m) {
-                return $this->_op($m[1], $m[2]);
-            },
-         ];
+            '/(\d{2}|)([MPmplrc])/' => fn($m) => $this->_op($m[1], $m[2]),
+        ];
 
         foreach ($repl as $pattern => $func) {
             $str = preg_replace_callback($pattern, $func, $str);
         }
 
         $trans = [
-            'E'  => 'printf("%c", $d[$i]);',
-            'l'  => 'for (;$d[$i];--$i);',
-            'r'  => 'for (;$d[$i];++$i);',
-            ','  => 'if (!$in) { $in = array_values(unpack("c*", rtrim(fgets(STDIN)))); $in[]=0; }; '.
-                    '$d[$i] = array_shift($in);',
-            'L'  => 'while ($d[$i]) {',
-            'R'  => '}',
-            '#'  => 'echo "$i: $d[$i]\n";',
-            'Y'  => '$pid = pcntl_fork(); if ($pid) $d[$i++] = 0; else $d[$i] = 1;',
+            'E' => 'printf("%c", $d[$i]);',
+            'l' => 'for (;$d[$i];--$i);',
+            'r' => 'for (;$d[$i];++$i);',
+            ',' => 'if (!$in) { $in = array_values(unpack("c*", rtrim(fgets(STDIN)))); $in[]=0; }; ' .
+                '$d[$i] = array_shift($in);',
+            'L' => 'while ($d[$i]) {',
+            'R' => '}',
+            '#' => 'echo "$i: $d[$i]\n";',
+            'Y' => '$pid = pcntl_fork(); if ($pid) $d[$i++] = 0; else $d[$i] = 1;',
         ];
 
-        if (in_array('Y', $this->opts) && strpos($str, 'Y') !== false) {
-            $yend = 'if ($pid) pcntl_wait($status);';
-        } else {
-            $yend = '';
-        }
-
-        return strtr($str, $trans).$yend;
+        return strtr($str, $trans);
     }
-     // }}}
-
-    // }}}
 }
-// }}}
